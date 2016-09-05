f6e010c5fc00f79369d6c94dbf1aa81b21511f9d
Casey M <caseym@gmail.com>
2016-09-05 17:09:10 -0400

    basic working flask app

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from models import Hotel
+from config import SQLALCHEMY_DATABASE_URI
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+
+db = SQLAlchemy()
 
 
 @app.route('/')
 def index():
+    # hotels = Hotel.query.all()
     return render_template('index.html')
 
 if __name__ == '__main__':
