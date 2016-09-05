49b98aa026a9ac0a70205029d32417d73f59c01a
Casey M <caseym@gmail.com>
2016-09-05 18:47:33 -0400

    using db within app.py

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-from models import Hotel
-from config import SQLALCHEMY_DATABASE_URI
+from models import Rate, Hotel, Location, db_setup
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
-
-db = SQLAlchemy()
 
 
 @app.route('/')
 def index():
-    # hotels = Hotel.query.all()
-    return render_template('index.html')
+    session = db_setup()
+    hotels = session.query(Hotel).all()
+    session.close()
+    return render_template('index.html', hotels=hotels)
 
 if __name__ == '__main__':
     app.run()
--- models.py
+++ models.py
@@ -42,7 +42,7 @@ class Location(Base):
 # db operations
 def db_setup():
     engine = create_engine(config.SQLALCHEMY_DATABASE_URI)
-    db_create_tables(engine)
+    # db_create_tables(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
     return session
