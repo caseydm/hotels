b6ac34d1d3ca8562a47e9d286ebd55be034b35d4
Casey M <caseym@gmail.com>
2016-09-06 19:06:25 -0400

    index changed to locations

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from models import Rate
+from models import Rate, Location
 
 # app setup
 app = Flask(__name__)
@@ -10,8 +10,9 @@ db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
-    rates = db.session.query(Rate).filter_by(hotel_id=1)
-    return render_template('index.html', rates=rates)
+    # rates = db.session.query(Rate).filter_by(hotel_id=1)
+    locations = db.session.query(Location).all()
+    return render_template('index.html', locations=locations)
 
 
 if __name__ == '__main__':
