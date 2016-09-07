0e0e0e52b4e5b7db74239ba8a915585cef741a4a
Casey M <caseym@gmail.com>
2016-09-06 21:22:05 -0400

    database changes

--- models.py
+++ models.py
@@ -44,7 +44,7 @@ class Rate(Base):
 # db operations
 def db_setup():
     engine = create_engine(config.SQLALCHEMY_DATABASE_URI)
-    Base.metadata.drop_all(engine)
+    # Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
