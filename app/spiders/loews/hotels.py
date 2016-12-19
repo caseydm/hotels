05b98e1902d9c9469a35366d0d915101461037bc
Casey M <caseym@gmail.com>
2016-12-19 07:29:27 -0500

    added new hotel

--- app/spiders/loews/hotels.py
+++ app/spiders/loews/hotels.py
@@ -1,3 +1,4 @@
 LOEWS_TEST = [
-    {'property_code': 'LATL', 'name': 'Loews Atlanta Hotel', 'city': 'Atlanta, GA', 'url_code': 'atlanta-hotel'}
+    {'property_code': 'LATL', 'name': 'Loews Atlanta Hotel', 'city': 'Atlanta, GA', 'url_code': 'atlanta-hotel'},
+    {'property_code': 'LANN', 'name': 'Loews Annapolis Hotel', 'city': 'Annapolis, MD', 'url_code': 'annapolis'}
 ]
