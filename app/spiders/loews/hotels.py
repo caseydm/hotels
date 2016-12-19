efcf7b22d81c29804bcfd3a24ed5613bbec20ced
Casey M <caseym@gmail.com>
2016-12-19 17:35:17 -0500

    added more hotels

--- app/spiders/loews/hotels.py
+++ app/spiders/loews/hotels.py
@@ -1,4 +1,7 @@
-LOEWS_TEST = [
+LOEWS1 = [
     {'property_code': 'LATL', 'name': 'Loews Atlanta Hotel', 'city': 'Atlanta, GA', 'url_code': 'atlanta-hotel'},
-    {'property_code': 'LANN', 'name': 'Loews Annapolis Hotel', 'city': 'Annapolis, MD', 'url_code': 'annapolis'}
+    {'property_code': 'LANN', 'name': 'Loews Annapolis Hotel', 'city': 'Annapolis, MD', 'url_code': 'annapolis'},
+    {'property_code': 'LVCR', 'name': 'Loews Ventana Canyon Resort', 'city': 'Tucson, AZ', 'url_code': 'ventana-canyon'},
+    {'property_code': 'LBBB', 'name': 'Loews Boston Hotel', 'city': 'Boston, MA', 'url_code': 'boston-hotel'},
+    {'property_code': 'LCHI', 'name': 'Loews Chicago Hotel', 'city': 'Chicago, IL', 'url_code': 'chicago-downtown'}
 ]
--- app/spiders/loews/loews1.py
+++ app/spiders/loews/loews1.py
@@ -1,4 +1,4 @@
 from app.spiders.loews.loews_scrape import scrape_loews
-from app.spiders.loews.hotels import LOEWS_TEST
+from app.spiders.loews.hotels import LOEWS1
 
-scrape_loews(LOEWS_TEST)
+scrape_loews(LOEWS1)
